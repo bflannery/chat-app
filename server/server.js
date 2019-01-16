d538dfe5e14b89f4fe55a4091456c308261e66e7
Brian Flannery <bflannery66@email.com>
2019-01-16 15:51:18 -0600

    added validation and testing

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const socketIO = require("socket.io");
 
 const { generateMessage, generateLocationMessage } = require('./utils/message');
+const { isRealString } = require('./utils/validation');
 
 const publicPath  = path.join(__dirname, "../public");
 
@@ -27,6 +28,18 @@ io.on('connection', (socket) => {
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+  socket.on('join', (params, callback) => {
+    console.log({name: params.name, room: params.room})
+    console.log({
+      nameIsRealString: isRealString(params.name),
+      roomIsRealString: isRealString(params.room)
+    })
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      callback('Name and room name are required.');
+    }
+    callback();
+  });
 
   socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
---
+++ server/utils/validation.js
@@ -0,0 +1,4 @@
+var isRealString = str => (typeof str === 'string' && str.trim().length > 0);
+
+
+module.exports = { isRealString }
